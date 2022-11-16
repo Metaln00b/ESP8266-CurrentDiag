2af73b64a401c58dc3a8219583e8cc49662f4363
Florian <Metaln00b@web.de>
2022-11-17 00:53:50 +0100

    Refactoring UDP

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "LittleFS.h"
 #include "Adafruit_INA219.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <Arduino_JSON.h>
@@ -16,11 +17,12 @@ AsyncEventSource events("/events");
 JSONVar readings;
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 500;
+unsigned long webTimerDelay = 500;
+unsigned long udpTimerDelay = 40;
 
 Adafruit_INA219 ina219;
 
-IPAddress multicastIP(230, 120, 10, 1);
+IPAddress broadcastIP(192, 168, 4, 255);
 constexpr uint16_t PORT = 8266;
 char packetBuffer[255];
 
@@ -28,8 +30,7 @@ WiFiUDP Udp;
 
 String getSensorReadings()
 {
-  //readings["sensor1"] = String(ina219.getCurrent_mA());
-  readings["sensor1"] = String(22);
+  readings["sensor1"] = String(ina219.getCurrent_mA());
 
   // Values from afr gauge documentation
   // minLambda = 0.683 (and offset)
@@ -45,8 +46,9 @@ String getSensorReadings()
 }
 
 void sendUdp() {
-  Udp.beginPacketMulticast(multicastIP, PORT, WiFi.localIP());
-  Udp.print(getSensorReadings());
+  Udp.beginPacket(broadcastIP, PORT);
+  String data = getSensorReadings();
+  Udp.print(data);
   Udp.endPacket();
 }
 
@@ -62,12 +64,70 @@ void initLittleFS()
   }
 }
 
+void showClients() 
+{
+  unsigned char number_client;
+  struct station_info *stat_info;
+  
+  struct ip4_addr *IPaddress;
+  IPAddress address;
+  int cnt=1;
+  
+  number_client = wifi_softap_get_station_num();
+  stat_info = wifi_softap_get_station_info();
+  
+  Serial.print("Connected clients: ");
+  Serial.println(number_client);
+
+  while (stat_info != NULL)
+  {
+      IPaddress = &stat_info->ip;
+      address = IPaddress->addr;
+
+      Serial.print(cnt);
+      Serial.print(": IP: ");
+      Serial.print((address));
+      Serial.print(" MAC: ");
+
+      uint8_t *p = stat_info->bssid;
+      Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X", p[0], p[1], p[2], p[3], p[4], p[5]);
+
+      stat_info = STAILQ_NEXT(stat_info, next);
+      cnt++;
+      Serial.println();
+  }
+}
+
+void eventCb(System_Event_t *evt)
+{
+    switch (evt->event)
+    {
+      case WIFI_EVENT_SOFTAPMODE_DISTRIBUTE_STA_IP:
+      case WIFI_EVENT_SOFTAPMODE_STADISCONNECTED:
+      showClients();
+      break;
+      
+    default:
+        break;
+    }
+}
+
 void initWiFi()
 {
-  WiFi.mode(WIFI_AP);
-  Serial.print("Setting soft-AP ... ");
-  
-  WiFi.softAP(ssid, NULL, 8);
+  Serial.println("Setting soft-AP ... ");
+  wifi_set_event_handler_cb(eventCb);
+
+  IPAddress apIP(192, 168, 4, 1);
+
+  if (WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0)))
+    Serial.println("softAPConfig: True");
+  else
+    Serial.println("softAPConfig: False");
+
+  if (WiFi.softAP(ssid, "123456789", 8))
+    Serial.println("softAP: True");
+  else
+    Serial.println("softAP: False");
   
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -102,7 +162,7 @@ void setup() {
 
   server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request)
   {
-    lastTime = lastTime + timerDelay;
+    lastTime = lastTime + webTimerDelay;
     request->send(200, "text/plain", "OK!");
   });
 
@@ -122,11 +182,11 @@ void setup() {
 
   server.begin();
 
-  Udp.beginMulticast(WiFi.softAPIP(), multicastIP, PORT);
+  Udp.begin(PORT);
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
+  if ((millis() - lastTime) > webTimerDelay) {
     events.send(getSensorReadings().c_str(),"new_readings" ,millis());
     lastTime = millis();
     sendUdp();
